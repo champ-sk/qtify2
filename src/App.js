4f093bc00aeab82f2bd1f806b50ef5bd8925b2da
champ-sk <shubhamkumar3183@gmail.com>
2024-11-26 11:11:28 +0530

    Adding Hero section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Hero from './Component/Hero/Hero';
 import Navbar from './Component/Navbar/Navbar';
 
 function App() {
  
   return (
     <div className="App">
-    
-
-     
-
      <Navbar/>
-  
+     <Hero />
 
     </div>
   );
