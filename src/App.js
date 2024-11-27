9b597104e8d48dd30ae9ceac1f66563683731c61
champ-sk <shubhamkumar3183@gmail.com>
2024-11-27 21:20:25 +0530

    Adding fianl changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-//import logo from './logo.svg';
-import './App.css';
-import Hero from './Component/Hero/Hero';
-import Navbar from './Component/Navbar/Navbar';
 
-function App() {
- 
+import "./App.css";
+import AlbumSongsPage from "./components/Album Songs Page/AlbumSongsPage";
+import HomePage from "./components/Home Page/HomePage";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-     <Navbar/>
-     <Hero />
-
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/album/:title" element={<AlbumSongsPage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
